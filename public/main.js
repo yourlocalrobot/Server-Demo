18a22f28abdffa2544a4051f3e7d68f7bfdd7e0f
benj <104083729+yourlocalrobot@users.noreply.github.com>
2023-10-17 23:20:24 -0400

    Cleanup

--- public/main.js
+++ public/main.js
@@ -1,35 +1,33 @@
+// Socket.io client setup
 const socket = io();
 
-// Create canvas context and set initial configurations
+// Canvas and context setup
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = "blue";
 
+// Initial player configuration
 let playerX = 50;
 let playerY = 50;
 
+// Data structures to hold other players and NPCs
 let otherPlayers = {};
-
 let npcs = [];
 
-socket.on("updateNPCs", (updatedNPCs) => {
-  npcs = updatedNPCs;
-  drawAllEntities();  // We'll define this function to redraw everything.
-});
+// Initialize canvas with a black background
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-function drawNPC(npc) {
-  ctx.beginPath();
-  ctx.fillStyle = "green";  // For example, NPCs can be in green.
-  ctx.arc(npc.x, npc.y, 25, 0, Math.PI * 2);  // Assuming the NPCs are also represented as circles.
-  ctx.fill();
-}
-
-// Draw the initial player position
+// Initial draw for the player
 drawPlayer(playerX, playerY);
 
-document.addEventListener("keydown", (event) => {
+// Event listeners
+document.addEventListener("keydown", handleKeydown);
+document.addEventListener("mousedown", (event) => {
+  event.preventDefault();
+});
+
+// Event handler for keypress
+function handleKeydown(event) {
   switch (event.code) {
     case "ArrowUp":
       playerY -= 5;
@@ -44,31 +42,50 @@ document.addEventListener("keydown", (event) => {
       playerX += 5;
       break;
   }
-
-  // Emit the new player position to the server
   socket.emit('updatePlayerPosition', { x: playerX, y: playerY });
+  drawAllEntities();
+}
 
-  // Redraw canvas
-   drawAllEntities();
-});
-
-document.addEventListener("mousedown", function(event){
-  event.preventDefault();
-});
-
+// Drawing functions
 function drawPlayer(x, y) {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath();  // Start a new path
   ctx.fillStyle = "blue";
+  ctx.beginPath();
   ctx.arc(x, y, 25, 0, Math.PI * 2);
   ctx.fill();
 }
 
-// Listen for other players' position updates
+function drawOtherPlayer(x, y) {
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(x, y, 25, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function drawNPC(npc) {
+  ctx.fillStyle = "green";
+  ctx.beginPath();
+  ctx.arc(npc.x, npc.y, 25, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function drawAllEntities() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  drawPlayer(playerX, playerY);
+  
+  for (let playerId in otherPlayers) {
+    let player = otherPlayers[playerId];
+    drawOtherPlayer(player.x, player.y);
+  }
+  
+  for (let npc of npcs) {
+    drawNPC(npc);
+  }
+}
+
+// Socket listeners
 socket.on("updatePlayers", (players) => {
   otherPlayers = players;
-  // Exclude own player data using socket.id, if needed
   delete otherPlayers[socket.id];
   drawAllEntities();
 });
@@ -78,46 +95,7 @@ socket.on("updatePlayer", (playerData) => {
   drawAllEntities();
 });
 
-function drawAllPlayers() {
-  // Clear canvas
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Draw main player
-  drawPlayer(playerX, playerY);
-
-  // Draw other players
-  for (let playerId in otherPlayers) {
-    let player = otherPlayers[playerId];
-    drawOtherPlayer(player.x, player.y);
-  }
-}
-
-function drawOtherPlayer(x, y) {
-  ctx.beginPath();
-  ctx.fillStyle = "red";  // You can choose a different color for other players
-  ctx.arc(x, y, 25, 0, Math.PI * 2);
-  ctx.fill();
-}
-
-function drawAllEntities() {
-  // Clear canvas
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Draw main player
-  drawPlayer(playerX, playerY);
-
-  // Draw other players
-  for (let playerId in otherPlayers) {
-    let player = otherPlayers[playerId];
-    drawOtherPlayer(player.x, player.y);
-  }
-
-  // Draw NPCs
-  for (let npc of npcs) {
-    drawNPC(npc);
-  }
-}
-
-
+socket.on("updateNPCs", (updatedNPCs) => {
+  npcs = updatedNPCs;
+  drawAllEntities();
+});
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+// Environment variables setup
 require('dotenv').config();
 
+// Imports
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const mysql = require('mysql');
 const path = require('path');
 
+// Express setup
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const port = 3000;
 
-// Initialize MySQL connection
+// Initialize MySQL connection using environment variables
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -19,6 +22,7 @@ const db = mysql.createConnection({
   multipleStatements: true
 });
 
+// Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to MySQL
@@ -26,17 +30,16 @@ db.connect((err) => {
   if (err) throw err;
   console.log('Connected to the database.');
 
-  // Create database if it doesn't exist
-  let createDbQuery = 'CREATE DATABASE IF NOT EXISTS gameDB;';
-  db.query(createDbQuery, (err, result) => {
+  // Ensure the database 'gameDB' exists
+  db.query('CREATE DATABASE IF NOT EXISTS gameDB;', (err) => {
     if (err) throw err;
 
-    // Use the database for future queries
-    db.query('USE gameDB', (err, result) => {
+    // Switch to the 'gameDB' database
+    db.query('USE gameDB', (err) => {
       if (err) throw err;
 
-      // Create tables if they don't exist
-      let createTableQuery = `
+      // Ensure the 'npc' table exists within 'gameDB'
+      const createTableQuery = `
         CREATE TABLE IF NOT EXISTS npc (
           id INT AUTO_INCREMENT,
           name VARCHAR(50),
@@ -44,47 +47,44 @@ db.connect((err) => {
           sprite_url VARCHAR(255),
           PRIMARY KEY(id)
         );`;
-      db.query(createTableQuery, (err, result) => {
+      db.query(createTableQuery, (err) => {
         if (err) throw err;
       });
     });
   });
 });
 
-// Empty arrays for NPCs and players
-let npcs = [
+// Initialize game data
+const npcs = [
   { name: "NPC1", stats: {}, x: 100, y: 100 }
-  // Add more NPCs here
 ];
-let players = {};
+const players = {};
 
+// Socket.io event handling
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  console.log("Emitting NPCs data:", npcs);  // Debugging log
+  // Send initial NPC data to connected client
   socket.emit("updateNPCs", npcs);
 
-  // Listen for playerMove event
+  // Update player position on move event
   socket.on("playerMove", (data) => {
-	console.log(`Received playerMove event from ${socket.id} with data:`, data); // Debugging log
     players[socket.id] = { x: data.x, y: data.y };
   });
 
-  // Handle 'updatePlayerPosition' event
+  // Broadcast updated player's position to others
   socket.on("updatePlayerPosition", (data) => {
-    // Update the player's position
     players[socket.id] = { x: data.x, y: data.y };
-
-    // Broadcast updated player's position to other clients
     socket.broadcast.emit("updatePlayer", { id: socket.id, x: data.x, y: data.y });
   });
 
+  // Broadcast chat messages to all clients
   socket.on("chatMessage", (message) => {
     io.emit("newChatMessage", message);
   });
 });
 
-// Update NPC positions periodically
+// Periodically update NPC positions
 setInterval(() => {
   npcs.forEach((npc) => {
     npc.x += Math.floor(Math.random() * 11) - 5;
@@ -93,15 +93,17 @@ setInterval(() => {
   io.emit("updateNPCs", npcs);
 }, 1000);
 
-// Update players periodically
+// Periodically update players to clients
 setInterval(() => {
   io.emit("updatePlayers", players);
 }, 1000);
 
+// Default route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start the server
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
